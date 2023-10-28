6af4811ff0b57f19f69e78a18f63ad3e6842e35a
fullstackdev225 <marchenrynanguy1@gmail.com>
2023-10-28 08:13:02 +0200

    feat(database): db interactions

---
+++ database/WordDb.go
@@ -0,0 +1,86 @@
+package database
+
+import (
+	"github.com/dgraph-io/badger/v4"
+	"marckent/dictionary/domain"
+	"marckent/dictionary/shared"
+)
+
+type WordDb struct {
+	db *badger.DB
+}
+
+func (w *WordDb) Save(entry domain.WordEntry) (err error) {
+	err = w.db.Update(func(txn *badger.Txn) error {
+		entryBuff := shared.EncodeStruct(entry)
+		err = txn.Set([]byte(entry.Word), entryBuff.Bytes())
+		return err
+	})
+
+	return
+}
+
+func (w *WordDb) Remove(word string) (err error) {
+	err = w.db.Update(func(txn *badger.Txn) error {
+		_, err := txn.Get([]byte(word))
+		if err != nil {
+			return err
+		}
+
+		err = txn.Delete([]byte(word))
+		return err
+	})
+
+	return
+}
+
+func (w *WordDb) GetOne(word string) (result domain.WordEntry, err error) {
+	err = w.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(word))
+		if err != nil {
+			return err
+		}
+
+		err = item.Value(func(v []byte) error {
+			result = shared.DecodeStruct[domain.WordEntry](v)
+			return nil
+		})
+
+		return err
+	})
+
+	return
+}
+
+func (w *WordDb) GetAll() (result []domain.WordEntry, err error) {
+	err = w.db.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchSize = 10
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		result, err = w.iterate(it)
+
+		return err
+	})
+
+	return
+}
+
+func (w *WordDb) iterate(it *badger.Iterator) (result []domain.WordEntry, err error) {
+
+	for it.Rewind(); it.Valid(); it.Next() {
+		item := it.Item()
+
+		err = item.Value(func(v []byte) error {
+			result = append(result, shared.DecodeStruct[domain.WordEntry](v))
+			return nil
+		})
+	}
+
+	return
+}
+
+func NewWordRepository(bd *badger.DB) *WordDb {
+	return &WordDb{db: bd}
+}
